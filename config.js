d9185e981c55dec57303fd809de1cf06e1935bac
Tanner Smoot <tanner@syno.utmin.com>
2026-07-22 13:11:33 -0600

    Feature July 30 demineralized water article

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.dashboardConfig = {
   // Optional: paste a published Google Sheet CSV URL here to update the site from a spreadsheet.
   // Leave blank to use data/blogs.js.
   googleSheetCsvUrl: "",
-  featuredArticleId: "are-all-trace-mineral-drops-the-same-what-anderson-gets-right",
+  featuredArticleId: "health-effects-of-drinking-demineralized-water",
 };
